c169f53ff9ebb70cbd27d18d1f4395ad9edbb124
Nohbdy <Billen93@outlook.com>
2017-05-31 11:13:56 +0200

    Yolo - Forgot some stuff

---
+++ It is a scary world/It is a scary world/ComponentPatteren/Door.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace It_is_a_scary_world
+{
+    class Door : Component
+    {
+        private Animator animator;
+        private IStrategy strategy;
+
+        public Door(GameObject gameObject) : base(gameObject)
+        {
+            gameObject.Tag = "Door";
+        }
+
+        public void LoadContent(ContentManager content, int xSize, int ySize)
+        {
+            animator = (Animator)gameObject.GetComponent("Animator");
+
+            Texture2D sprite = content.Load<Texture2D>("Door");
+
+            animator.CreateAnimation("IdleFront", new Animation(1, 0, 0, xSize, ySize, 0, Vector2.Zero, sprite));
+
+            animator.PlayAnimation("IdleFront");
+
+            strategy = new Idle(animator);
+        }
+    }
+}
---
+++ It is a scary world/It is a scary world/ObjectPool/DoorPool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace It_is_a_scary_world
+{
+    class DoorPool
+    {
+        private static List<GameObject> inactive = new List<GameObject>();
+
+        private static List<GameObject> active = new List<GameObject>();
+
+        private static Director director = new Director(new DoorBuilder());
+
+        public static GameObject Create(Vector2 position, ContentManager content, int xSize, int ySize)
+        {
+            if (inactive.Count > 0)
+            {
+                GameObject door = inactive[0];
+                active.Add(door);
+                inactive.RemoveAt(0);
+
+                return door;
+            }
+            else
+            {
+
+                GameObject door = director.Construct(position);
+
+                (door.GetComponent("Door") as Door).LoadContent(content, xSize, ySize);
+
+                door.LoadContent(content);
+
+                active.Add(door);
+
+                return door;
+            }
+        }
+
+        public static void ReleaseObject(GameObject door)
+        {
+            CleanUp();
+
+            inactive.Add(door);
+
+            active.Remove(door);
+        }
+
+        private static void CleanUp()
+        {
+            //Reset data, remove references etc.
+        }
+    }
+}
